ebd1c3acc49708137a724433ba5f24ddcd9a356d
kiddo <348653172@qq.com>
2023-03-11 23:35:05 +0800

    fix setting bugs

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,8 +2,8 @@ import app from 'flarum/admin/app';
 import Doorkey from './models/Doorkey';
 import SettingsPage from './components/SettingsPage';
 
-app.initializers.add('fof-doorman', () => {
+app.initializers.add('kiddo-doorman', () => {
   app.store.models.doorkeys = Doorkey;
 
-  app.extensionData.for('fof-doorman').registerPage(SettingsPage);
+  app.extensionData.for('kiddo-doorman').registerPage(SettingsPage);
 });
